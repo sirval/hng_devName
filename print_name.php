8440d102e5d42fe9c9fbca5132e9e1d41500af6c
sirval <20794595+sirval@users.noreply.github.com>
2021-08-19 14:28:12 -0800

    Add files via upload

---
+++ print_name.php
@@ -0,0 +1,31 @@
+<?php
+	$devName = 'Ohuka Ikenna Valentine';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Stage Two | Task 2</title>
+	<style type="text/css">
+		body{
+			background-color: #546e7a;
+		}
+		h1{
+			text-align: center;
+			color: #fff
+		}
+		p{
+			text-align: center;
+			color: #fff
+		}
+
+	</style>
+</head>
+<body>
+	<h1>HNGi8xi4G Internship</h1>
+	<p>
+		<?php
+			echo "My name is ".$devName;
+		?>
+	</p>
+</body>
+</html>
